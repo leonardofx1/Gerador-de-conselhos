1f2d8d53894707be2b78cfd2a025978c8ab2a65f
leonardofx1 <leonardonunesfreitas1@gmail.com>
2023-01-16 16:28:15 -0300

    'msg error'

--- JS/login.js
+++ JS/login.js
@@ -1,30 +1,47 @@
 const form = document.querySelector('.form')
 const email = document.querySelector('#email')
 const password = document.querySelector('#password')
+const msgError = document.querySelector('#msg')
 
-
+console.log(msgError)
 const validacaoForm = (val,regex) =>  regex.test(val)
 
 const validForm = (input)=> {
             input.classList.remove('error')
-            input.classList.add('success')    
+            input.classList.add('success')
 }
 
 const erorrForm = input => {
    input.classList.remove('success')
    input.classList.add('error')
 }
+const passErro = () => {
+    if(Array.from(msgError.classList).includes('none')){
+        msgError.classList.remove('none')
+    }
+    msgError.classList.add('msg__error')
+    console.log(msgError.classList)
+}
+const passSuccess = () => {
+    msgError.classList.remove('msg__error')
+    msgError.classList.add('none')
+}
+
+const inputClass = target => {
+    target.setAttribute('class', 'input')
+    }
 
 password.addEventListener('input', ({target})=> {
     const passwordChecked = validacaoForm(target.value,/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{8,})$/)
-    passwordChecked && validForm(target)
-    !passwordChecked && erorrForm(target)
-    target.value === '' && inputClass(target)
+    passwordChecked && validForm(target) || passSuccess()
+    if(!passwordChecked) { 
+        erorrForm(target) 
+        passErro()
+    }
+    target.value === '' && inputClass(target) 
 })
 
-const inputClass = target => {
-    target.value === '' && target.setAttribute('class', 'input')
-}
+
 
 email.addEventListener('input', ({target})=> {
     const emailChecked = validacaoForm(target.value,/^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i)

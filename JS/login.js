57784c1ad686da88682b3fd9ee06e45aec4c5c82
leonardofx1 <leonardonunesfreitas1@gmail.com>
2023-01-16 11:56:15 -0300

    login

--- JS/login.js
+++ JS/login.js
@@ -18,19 +18,27 @@ const erorrForm = input => {
 password.addEventListener('input', ({target})=> {
     const passwordChecked = validacaoForm(target.value,/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{8,})$/)
     passwordChecked && validForm(target)
-    !passwordChecked && erorrForm(target) 
+    !passwordChecked && erorrForm(target)
+    target.value === '' && inputClass(target)
 })
 
+const inputClass = target => {
+    target.value === '' && target.setAttribute('class', 'input')
+}
 
 email.addEventListener('input', ({target})=> {
-    const input = target
+    const emailChecked = validacaoForm(target.value,/^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i)
+    emailChecked && validForm(target) 
+    !emailChecked && erorrForm(target)
+    target.value ==='' && inputClass(target)
 
-    const emailChecked = validacaoForm(input.value,/^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i)
-    emailChecked ? validForm(input) : erorrForm(input)
 })
 
 form.addEventListener('submit', e => {
     e.preventDefault()
+
+    if(Array.from(email.classList).includes('success') &&Array.from( password.classList).includes('success') ){
+         location.href = 'http://127.0.0.1:5500/home/index.html'
+    }
     form.reset()
-  
 })
